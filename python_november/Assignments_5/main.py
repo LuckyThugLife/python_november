ad5ef5297003000f2b2d83a7eba5ee7d6c01a4e8
Rustam Sharipov <rustam87.sharipov@gmail.com>
2023-11-19 11:17:48 +0500

    adding the assignment 5

---
+++ python_november/Assignments_5/main.py
@@ -0,0 +1,24 @@
+# Code 1
+if 10 > 5:
+    print("Ten is greater than five!") # On this line, I added an indent at the beginning, 
+    # because according to the rules of the Python language, 
+    # that indented line fulfills the condition of the top line.
+
+# Code 2
+if 8 > 3:
+    print("Eight is greater than three!")
+    print("Indeed!") # There are no errors in this code,
+    # I can only emphasize that the code is executed from top to bottom.
+
+# Code 3
+a = 7
+b = 3
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater than a") # There are no errors in this code,
+"""
+In this code , integer type values are assigned to variables a and b ,
+ then they are compared using comparison operators .
+"""
+
