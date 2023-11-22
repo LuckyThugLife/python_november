9c9e9c21551410d685b06fc5ecf845102cac8867
Rustam Sharipov <rustam87.sharipov@gmail.com>
2023-11-22 16:57:28 +0500

    Assignments_6

---
+++ python_november/Assignments_6/python_numbers.py
@@ -0,0 +1,27 @@
+# Scenario 1: Numbers and Casting
+# a. 
+
+num1 = 15.6
+num2 = 8
+
+print(num1)
+print(num2)
+
+# b. Converting the num2 to a floating point number 
+# and multiplying it by the num1. Output the result.
+
+num2 = float(num2)
+print(num2)
+
+print(num1 * num2)
+
+# c.
+# Type conversion in Python is performed using constructor functions:
+# int() - creates an integer from an integer literal, a floating—point literal 
+# (by removing all decimal places);
+# float() — creates a floating—point number from an integer literal, a floating
+# -point literal str() - creates a string from a variety of data types.
+
+# in example a, we output the values of variables using the print() function
+# in example b, we convert num2 to a floating-point number using the float() function 
+# and multiply it by num1 using the multiplication operator. We output the result.
---
+++ python_november/Assignments_6/python_strings.py
@@ -0,0 +1,27 @@
+# Scenario 2: String Manipulation
+# a. Output of the message length.
+
+message = "Hello, Python learners!"
+
+print(len(message))
+
+# b.
+print(message.lower())
+
+# c.
+print(message[7:13])
+
+# d.
+# The len() function returns the length of the string:
+example = "Hi, bro!"
+print(len(example))
+
+# The lower() method returns a string in lowercase:
+example = "Hi, bro!"
+print(example.lower())
+
+# unlike a function where we put a variable 
+# in the body of a function, we access the method through a dot.
+
+# strings in Python are arrays, with the help of square brackets 
+# you can use access to the elements of the string as we did in the example c.
