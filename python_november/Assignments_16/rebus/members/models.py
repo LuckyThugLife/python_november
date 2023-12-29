a0a8cc3fdb825cdc182ba616dcfd86c2711e3400
Rustam Sharipov <rustam87.sharipov@gmail.com>
2023-12-29 22:51:07 +0500

    Added assignment 19

--- python_november/Assignments_16/rebus/members/models.py
+++ python_november/Assignments_16/rebus/members/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Cities(models.Model):
+    name = models.CharField(max_length=295)
+    description = models.TextField(blank=True)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=295)
+    description = models.TextField(blank=True)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+class Friends(models.Model):
+    name = models.CharField(max_length=295)
+    description = models.TextField(blank=True)
+    added_date = models.DateTimeField(auto_now_add=True)
