bb95746cc04e07f4627f6317a7a60773ca659b9f
Rustam Sharipov <rustam87.sharipov@gmail.com>
2024-01-11 14:51:55 +0500

    adding image and some better styles

--- python_november/Assignments_16/rebus/rebus/settings.py
+++ python_november/Assignments_16/rebus/rebus/settings.py
@@ -114,6 +114,10 @@ STATIC_ROOT = BASE_DIR / 'productionfiles'
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystaticfiles'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
